cc7e26cf7edf4711e050c9a22cc19ac4601184aa
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-09-28 23:08:46 +0200

    Small changes.

--- src/app/_components/organisms/trackersTable/TrackersTable.tsx
+++ src/app/_components/organisms/trackersTable/TrackersTable.tsx
@@ -4,8 +4,8 @@ import { DataTable } from "primereact/datatable";
 function TrackersTableActions() {
   return (
     <div className="flex w-full items-center justify-start gap-3">
-      <i className="pi pi-pause cursor-pointer text-primary lg:transition-opacity lg:hover:opacity-70"></i>
-      {/* pi-play */}
+      <i className="pi pi-play cursor-pointer text-primary lg:transition-opacity lg:hover:opacity-70"></i>
+      {/* pi-play pi-pause */}
       <i className="pi pi-stop-circle cursor-pointer lg:transition-opacity lg:hover:opacity-70"></i>
       <i className="pi pi-pencil cursor-pointer lg:transition-opacity lg:hover:opacity-70"></i>
       <i className="pi pi-trash cursor-pointer lg:transition-opacity lg:hover:opacity-70"></i>
@@ -23,7 +23,8 @@ export default function TrackersTable() {
     {
       timeLogged: "01:30:33",
       dateCreated: "21/09/2023",
-      description: "This is a description",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat recusandae veritatis illum et, quibusdam minus sint distinctio repellat libero iure ut temporibus eos unde facilis? Architecto aliquam perferendis distinctio ducimus magni fugit blanditiis sapiente libero numquam, ab nam, molestias voluptatibus beatae deleniti recusandae in eum. Ullam harum illum ratione exercitationem?",
     },
     {
       timeLogged: "01:30:33",
@@ -106,7 +107,6 @@ export default function TrackersTable() {
             backgroundColor: "rgb(228 228 231)",
           }}
         ></Column>
-        {/* <Column field="category" header="Category"></Column> */}
         <Column
           header="Actions"
           headerClassName="font-bold"
